5b73783c4b53f5f02cc800d7b2952a2c820b38e1
Steven Ireland <steven.ireland@protonmail.com>
2023-05-21 19:25:25 -0400

    Add project

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ const GITHUB_IMG_URL = 'https://avatars.githubusercontent.com/u/6981727?v=4';
 const GH_IMPROVEMENTML = 'https://github.com/Steven-Ireland/ImprovementML-Brain';
 const GH_KRAKENSHEET = 'https://github.com/Steven-Ireland/krakensheet.com';
 const GH_BLOG = 'https://github.com/Steven-Ireland/stevenire.land';
+const GH_SPAM = 'https://github.com/Steven-Ireland/spam';
 
 const About = ({ data }) => {
   return (
@@ -28,6 +29,7 @@ const About = ({ data }) => {
         Outside of work, I hack on a slew of projects including:
       </p>
       <ul>
+        <li>A tiny general-purpose load testing tool built in Go <a href={GH_SPAM}><Link size={12}/></a></li>
         <li>A Reinforcement Learning based Unity Game where fantastical creatures learn to walk <a href={GH_IMPROVEMENTML}><Link size={12}/></a></li>
         <li>A Next-Generation character sheet application for tabletop games built with React, Redux, and Node.js <a href={GH_KRAKENSHEET}><Link size={12}/></a></li>
         <li>A yet-unreleased GraphQL-first Content Management Platform, with Next.js and Apollo Server</li>
