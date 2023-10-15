80d0679c447356e5c8b65655fa1d1aadc8b88235
Steven Ireland <steven.ireland@protonmail.com>
2023-10-15 12:11:23 -0400

    update site

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ const GH_IMPROVEMENTML = 'https://github.com/Steven-Ireland/ImprovementML-Brain'
 const GH_KRAKENSHEET = 'https://github.com/Steven-Ireland/krakensheet.com';
 const GH_BLOG = 'https://github.com/Steven-Ireland/stevenire.land';
 const GH_SPAM = 'https://github.com/Steven-Ireland/spam';
+const BRICK_URL = 'https://brick-cms.com';
 
 const About = ({ data }) => {
   return (
@@ -19,7 +20,7 @@ const About = ({ data }) => {
       </div>
       
       <p>
-        I'm Steven Ireland, a lifelong Software Engineer most recently working as a Staff Engineer at Wayfair. 
+        I'm Steven Ireland, a lifelong Software Engineer most recently working as a Staff Engineer at Cadre. 
       </p>
       <p>
         In my day job I define high level system architecture, facilitate tough conversations and drive organizational alignment,
@@ -29,10 +30,10 @@ const About = ({ data }) => {
         Outside of work, I hack on a slew of projects including:
       </p>
       <ul>
+        <li>A GraphQL-first CMS platform, built with Next.js and Apollo Server <a href={BRICK_URL}><Link size={12}/></a></li>
         <li>A tiny general-purpose load testing tool built in Go <a href={GH_SPAM}><Link size={12}/></a></li>
         <li>A Reinforcement Learning based Unity Game where fantastical creatures learn to walk <a href={GH_IMPROVEMENTML}><Link size={12}/></a></li>
         <li>A Next-Generation character sheet application for tabletop games built with React, Redux, and Node.js <a href={GH_KRAKENSHEET}><Link size={12}/></a></li>
-        <li>A yet-unreleased GraphQL-first Content Management Platform, with Next.js and Apollo Server</li>
         <li>And this website you're reading right now! <a href={GH_BLOG}><Link size={12}/></a></li>
       </ul>
       <p>
